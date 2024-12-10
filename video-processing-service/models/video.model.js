b79f24a9922467a257ceb4d8bca3905180d12b54
YanisArar931 <yan.iss.arar@gmail.com>
2024-12-10 12:06:10 +0100

    fix model

--- video-processing-service/models/video.model.js
+++ video-processing-service/models/video.model.js
@@ -1,25 +1,3 @@
-// const mongoose = require('mongoose');
-
-// // Définir le schéma pour les vidéos
-
-// const videoSchema = new mongoose.Schema({
-//   filename: { type: String, required: true, unique: true },
-//   title: { type: String },
-//   description: { type: String },
-//   tags: { type: [String] },
-// });
-
-// // Middleware pour mettre à jour `updatedAt` avant chaque sauvegarde
-// videoSchema.pre('save', function (next) {
-//   this.updatedAt = Date.now();
-//   next();
-// });
-
-// // Créer un modèle basé sur le schéma
-// const Video = mongoose.model('Video', videoSchema);
-
-// module.exports = Video;
-
 const mongoose = require('mongoose');
 
 // Définir le schéma pour les vidéos
